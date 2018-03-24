1f7f0c3109b3832147bb25742be143503bfb12ec
jdlrobson <jdlrobson@gmail.com>
2018-03-24 09:58:21 -0700

    Show/hide children only shows when there are children

--- html/scripts.js
+++ html/scripts.js
@@ -1,9 +1,11 @@
+var HIDE_LABEL = 'hide children';
+
 Array.from( document.querySelectorAll( '.person__children' ) ).forEach(( block ) => {
   let btn = document.createElement('button');
-  btn.textContent = 'toggle';
+  btn.textContent = HIDE_LABEL;
   handler = (function (b) {
     return (ev) => {
-      console.log('toggle', b.style.display);
+      btn.textContent = b.style.display === '' ? 'show children' : HIDE_LABEL;
       b.style.display = b.style.display === '' ? 'none' : '';
     };
   }(block));
